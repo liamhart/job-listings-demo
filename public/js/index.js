af0f6f24ae6720a5fbfdad572c79dbb206fcc2e5
Liam Hart <liamh026@gmail.com>
2020-07-22 15:39:52 -0400

    search algorithm improved

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,57 @@
+
+
 $(document).ready(function() {
+
+  function Listing(data) {
+    this.id = data.id;
+    this.company = data.company;
+    this.logo = data.logo;
+    this.new = data.new;
+    this.featured = data.featured;
+    this.position = data.position;
+    this.role = data.role;
+    this.level = data.level;
+    this.postedAt = data.postedAt;
+    this.contract = data.contract;
+    this.location = data.location;
+    this.languages = data.languages;
+    this.tools = data.tools;
+    this.tags = [].concat(data.role, data.level, data.languages, data.tools)
+  }
+
+
   $.getJSON('data.json', function(data) {
+    var Listings = []
     // structure page from JSON data
     for(var i = 0; i < data.length; i++) {
+      Listings.push(new Listing(data[i]))
+    }
 
+    console.log(Listings)
+
+    for(var i = 0; i < Listings.length; i++) {
       // Determine if feature identifiers will be present in the listing
-      var feature_new = data[i].new ? '<p class="listing-status listing-new">NEW!</p>' : '';
-      var feature_featured = data[i].featured ? '<p class="listing-status listing-featured">FEATURED</p>' : '';
+      var feature_new = Listings[i].new ? '<p class="listing-status listing-new">NEW!</p>' : '';
+      var feature_featured = Listings[i].featured ? '<p class="listing-status listing-featured">FEATURED</p>' : '';
 
-      $('#job-feed').append('<div class="job-container"> <div class="row"> <img src="' + data[i].logo + '" alt="eyecam-co" class="listing-avatar"><div class="col-lg-4"> <div class="row listing-attributes"><p class="listing-company">' + data[i].company + '</p>' + feature_new + feature_featured + '</div><div class="row"><p class="listing-position">' + data[i].position + '</p></div><div class="row listing-info"><p class="listing-date-posted">' + data[i].postedAt + '</p><p class="listing-info-spacer">·</p><p class="listing-type">' + data[i].contract + '</p><p class="listing-info-spacer">·</p><p class="listing-location">' + data[i].location + '</p> </div></div><div class="col-lg-6"> <div class="row listing-tags listing-tags-' + data[i].id + '"></div></div></div></div>')
+      $('#job-feed').append('<div id="' + Listings[i].id + '" class="job-container"> <div class="row"> <img src="' + Listings[i].logo + '" alt="eyecam-co" class="listing-avatar"><div class="col-lg-4"> <div class="row listing-attributes"><p class="listing-company">' + Listings[i].company + '</p>' + feature_new + feature_featured + '</div><div class="row"><p class="listing-position">' + Listings[i].position + '</p></div><div class="row listing-info"><p class="listing-date-posted">' + Listings[i].postedAt + '</p><p class="listing-info-spacer">·</p><p class="listing-type">' + Listings[i].contract + '</p><p class="listing-info-spacer">·</p><p class="listing-location">' + Listings[i].location + '</p> </div></div><div class="col-lg-6"> <div class="row listing-tags listing-tags-' + Listings[i].id + '"></div></div></div></div>')
 
       // Determine if job listings will have featured flair
-      if (data[i].featured) {
+      if (Listings[i].featured) {
         $('.job-container').addClass('job-container-featured')
       }
       // Determine listing tags
-      if (data[i].role) {
-        $('.listing-tags-' + data[i].id).append('<p class="listing-tag">' + data[i].role + '</p>')
+      if (Listings[i].role) {
+        $('.listing-tags-' + Listings[i].id).append('<p class="listing-tag listing-tag-' + Listings[i].role + '">' + Listings[i].role + '</p>')
       }
-      if (data[i].level) {
-        $('.listing-tags-' + data[i].id).append('<p class="listing-tag">' + data[i].level + '</p>')
+      if (Listings[i].level) {
+        $('.listing-tags-' + Listings[i].id).append('<p class="listing-tag listing-tag-' + Listings[i].level + '">' + Listings[i].level + '</p>')
       }
-      if (data[i].languages) {
-        data[i].languages.forEach(e => $('.listing-tags-' + data[i].id).append('<p class="listing-tag">' + e + '</p>'))
+      if (Listings[i].languages) {
+        Listings[i].languages.forEach(e => $('.listing-tags-' + Listings[i].id).append('<p class="listing-tag listing-tag-' + e + '">' + e + '</p>'))
       }
-      if (data[i].tools) {
-        data[i].tools.forEach(e => $('.listing-tags-' + data[i].id).append('<p class="listing-tag">' + e + '</p>'))
+      if (Listings[i].tools) {
+        Listings[i].tools.forEach(e => $('.listing-tags-' + Listings[i].id).append('<p class="listing-tag listing-tag-' + e + '">' + e + '</p>'))
       }
 
     }
@@ -41,28 +68,35 @@ $(document).ready(function() {
 
     // this will hold all active queries
     var queries = [];
-      $('.listing-tag').click(function() {
-        if(queries.includes($(this).text())) {
-          // remove existing query
-          queries.splice(queries.indexOf($(this).text()), 1)
-          // find specific search tag related to the existing query
-          $('.search-tag-' + $(this).text()).addClass('removed')
+    $('.listing-tag').click(function() {
+      if(queries.includes($(this).text())) {
+        // remove existing query
+        queries.splice(queries.indexOf($(this).text()), 1)
+        // find specific search tag related to the existing query
+        $('.search-tag-' + $(this).text()).addClass('removed')
+        // any job listings that were not included in the search should re-appear
+        for(var i = 0; i < Listings.length; i++) {
+          if(queries.every(r => Listings[i].tags.includes(r))) {
+            $('#' + Listings[i].id + '.job-container').css('display', 'block')
+          }
         }
-        else {
-          // add new query
-          queries.push($(this).text())
-          // add search tag related to user query
-          $('.search-tags').append('<div class="query-wrapper search-tag-' + $(this).text() + '"> <div class="listing-search"> <p>' + $(this).text() + '</p></div><div class="remove-search"> <span class="search-cross"></span> </div></div>')
+      }
+      else {
+        // add new query
+        queries.push($(this).text())
+        // add search tag related to user query
+        $('.search-tags').append('<div class="query-wrapper search-tag-' + $(this).text() + '"> <div class="listing-search"> <p>' + $(this).text() + '</p></div><div class="remove-search"> <span class="search-cross"></span> </div></div>')
 
-        }
-        if(queries.length) {
-          $('.search-bar').css('visibility', 'visible')
-        }
-        else {
-          $('.search-bar').css('visibility', 'hidden')
-        }
-        console.log(queries)
-      })
+        $('.job-container').not(':has(.listing-tag-' + $(this).text() + ')').css('display','none')
+      }
+      if(queries.length) {
+        $('.search-bar').css('visibility', 'visible')
+      }
+      else {
+        $('.search-bar').css('visibility', 'hidden')
+      }
+      console.log(queries)
+    })
 
   })
 })
