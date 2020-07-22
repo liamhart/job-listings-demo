ff30c80968d9675bbaae70b6e8d1a8d41ce72ded
Liam Hart <liamh026@gmail.com>
2020-07-22 11:19:27 -0400

    port error fix

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const app = express();
-const port = 3000
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
-app.listen(port, () => console.log("Running on port: " + port))
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000.")
+})
